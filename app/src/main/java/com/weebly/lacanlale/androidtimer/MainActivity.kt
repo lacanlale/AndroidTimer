7412c6657c87d9e3be3005ddc793ad17d5c76a63
lacanlale <gizmo1398@gmail.com>
2018-05-05 19:03:38 -0700

    i made things pink for added cuteness

--- app/src/main/java/com/weebly/lacanlale/androidtimer/MainActivity.kt
+++ app/src/main/java/com/weebly/lacanlale/androidtimer/MainActivity.kt
@@ -1,29 +1,64 @@
 package com.weebly.lacanlale.androidtimer
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.os.CountDownTimer
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 
-import kotlinx.android.synthetic.main.activity_main.*
-
+/**
+ * Simple Timer app for all your timing needs!
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var timer: CountDownTimer
+    private lateinit var fabPause: FloatingActionButton
+    private lateinit var fabPlay: FloatingActionButton
+    private lateinit var fabStop: FloatingActionButton
+    private lateinit var time: TextView
+
+    private var timerLengthSeconds: Long = 0
+    private var timerState = TimerState.Stopped
+    private var secondsRemaining = 0L
+
+    enum class TimerState {
+        Stopped, Paused, Running
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "\t\t\t:･ﾟ☆✧*⏰TIMER⏰*✧☆ﾟ･:"
 
-        fab_play.setOnClickListener { view ->
-            Toast.makeText(applicationContext, ":･ﾟ☆✧ STARTING ✧☆ﾟ･:", Toast.LENGTH_SHORT).show()
+        fabPause = findViewById(R.id.fab_pause)
+        fabPlay = findViewById(R.id.fab_play)
+        fabStop = findViewById(R.id.fab_stop)
+        time = findViewById(R.id.tv_timer)
+
+        time.text = getString(R.string.timer, "00", "00", "00")
+
+        fabPause.setOnClickListener { _ ->
+            Toast.makeText(applicationContext, ":･ﾟ☆✧* PAUSING *✧☆ﾟ･:", Toast.LENGTH_SHORT).show()
+            timerState = TimerState.Paused
+            updateButtons()
+            Log.d("TIMER_STATE", "State: $timerState")
         }
-        fab_pause.setOnClickListener { _ ->
-            Toast.makeText(applicationContext, ":･ﾟ☆✧ PAUSING ✧☆ﾟ･:", Toast.LENGTH_SHORT).show()
+        fabPlay.setOnClickListener { _ ->
+            Toast.makeText(applicationContext, ":･ﾟ☆✧* STARTING *✧☆ﾟ･:", Toast.LENGTH_SHORT).show()
+            timerState = TimerState.Running
+            updateButtons()
+            Log.d("TIMER_STATE", "State: $timerState")
         }
-        fab_stop.setOnClickListener { view ->
-            Toast.makeText(applicationContext, ":･ﾟ☆✧ STOPPING ✧☆ﾟ･:", Toast.LENGTH_SHORT).show()
+        fabStop.setOnClickListener { _ ->
+            Toast.makeText(applicationContext, ":･ﾟ☆✧* STOPPING *✧☆ﾟ･:", Toast.LENGTH_SHORT).show()
+            timerState = TimerState.Stopped
+            updateButtons()
+            Log.d("TIMER_STATE", "State: $timerState")
         }
     }
 
@@ -36,6 +71,25 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun updateButtons() {
+        when (timerState) {
+            TimerState.Running -> {
+                fabPlay.isEnabled = false
+                fabPause.isEnabled = true
+                fabStop.isEnabled = true
+            }
+            TimerState.Stopped -> {
+                fabPlay.isEnabled = true
+                fabPause.isEnabled = false
+                fabStop.isEnabled = false
+            }
+            TimerState.Paused -> {
+                fabPlay.isEnabled = true
+                fabPause.isEnabled = false
+                fabStop.isEnabled = true
+            }
         }
     }
 }
